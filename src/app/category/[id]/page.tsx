e12ad1e38d82fed2c5d1330c2d68ba9a205a23e6
Sieep-Coding <nstamb6@gmail.com>
2024-05-03 00:00:02 -0700

    frontend work and routing

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import QuoteModal from '@/components/QuoteModal';
+import { useRouter } from 'next/navigation';
 
 interface CategoryPageProps {
   params: {
@@ -8,11 +11,22 @@ interface CategoryPageProps {
 
 export default function CategoryPage({ params }: CategoryPageProps) {
   const categoryId = parseInt(params.id);
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.push('https://philosophy-quotes.vercel.app/');
+  };
 
   return (
     <div className="container">
-      <h1 className="text-4xl font-bold mb-8">Inspirational Quotes</h1>
-      <QuoteModal categoryId={categoryId} />
+      <h1 className="text-4xl font-bold mb-4">Philosophy Quotes</h1>
+      <QuoteModal categoryId={categoryId} /> 
+      <br></br>
+      <button 
+      className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded"
+      onClick={handleGoBack}     >
+      ← Go Back
+      </button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import CategoryCard from '@/components/CategoryCard';
 
 const categories = [
-  { id: 1, name: 'Stoic', image: '/icons8-budgie-90.png' },
+  { id: 1, name: 'Stoic', image: '/icons8-crane-bird-90.png' },
   { id: 2, name: 'Brave', image: '/icons8-bull-90.png' },
-  { id: 3, name: 'Classic Philosophy', image: '/insperational.jpg' },
+  { id: 3, name: 'Classic Philosophy', image: '/icons8-turtle-90.png' },
   { id: 4, name: 'Good Life', image: '/goodlife.sv' },
   { id: 5, name: 'Courage', image: '/brave.sv' },
   // Add more categories as needed
