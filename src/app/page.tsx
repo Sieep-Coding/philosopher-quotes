a9a4f9aa562fd4d66995bfae2e59052c02d39c0d
Sieep-Coding <nstamb6@gmail.com>
2024-05-02 23:42:43 -0700

    frontend work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CategoryCard from '@/components/CategoryCard';
 
 const categories = [
   { id: 1, name: 'Stoic', image: '/icons8-budgie-90.png' },
-  { id: 2, name: 'Brave', image: '/brave.jpg' },
+  { id: 2, name: 'Brave', image: '/icons8-bull-90.png' },
   { id: 3, name: 'Classic Philosophy', image: '/insperational.jpg' },
   { id: 4, name: 'Good Life', image: '/goodlife.sv' },
   { id: 5, name: 'Courage', image: '/brave.sv' },
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl font-bold mb-8">Philosopher Quotes</h1>
-      <h3 className="text-2xl font-bold mb-3">Explore inspirational, inspiring and thought-provoking quotes.</h3>
+      <h3 className="text-2xl font-bold mb-3">Explore thought-provoking quotes.</h3>
       <div className="grid grid-cols-3 gap-4">
         {categories.map((category) => (
           <CategoryCard key={category.id} category={category} />
--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -13,17 +13,17 @@ interface CategoryCardProps {
 export default function CategoryCard({ category }: CategoryCardProps) {
   return (
     <Link href={`/category/${category.id}`}>
-      <div className="bg-gradient-to-br from-purple-500 to-pink-500 rounded-lg overflow-hidden shadow-md hover:shadow-lg cursor-pointer transform hover:scale-105 transition-all duration-300">
+      <div className="bg-gradient-to-br from-blue-300 to-purple-300 rounded-lg overflow-hidden border-2 border-blue-400 hover:border-purple-500 hover:shadow-md cursor-pointer transform hover:scale-105 transition-all duration-300 flex flex-col justify-center items-center">
         <img
           src={category.image}
           alt={category.name}
-          className="w-full h-40 object-cover"
+          className="w-35 h-36 object-cover"
         />
-        <div className="p-4">
-          <h2 className="text-xl font-semibold text-white mb-2">
+        <div className="p-4 text-center">
+          <h2 className="text-2xl font-bold text-gray-900 mb-2 tracking-wide capitalize">
             {category.name}
           </h2>
-          {/* <p className="text-sm text-gray-200">{category.description}</p> */}
+          {/* <p className="text-sm text-gray-600">{category.description}</p> */}
         </div>
       </div>
     </Link>
