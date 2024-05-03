8ff55d757badf0e86b76466744bbf8fd666c3ad3
Sieep-Coding <nstamb6@gmail.com>
2024-05-02 23:29:05 -0700

    frontend work

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -13,9 +13,18 @@ interface CategoryCardProps {
 export default function CategoryCard({ category }: CategoryCardProps) {
   return (
     <Link href={`/category/${category.id}`}>
-      <div className="bg-white shadow-md rounded-lg p-4 cursor-pointer">
-        <img src={category.image} alt={category.name} className="mb-4" />
-        <h2 className="text-xl font-semibold">{category.name}</h2>
+      <div className="bg-gradient-to-br from-purple-500 to-pink-500 rounded-lg overflow-hidden shadow-md hover:shadow-lg cursor-pointer transform hover:scale-105 transition-all duration-300">
+        <img
+          src={category.image}
+          alt={category.name}
+          className="w-full h-40 object-cover"
+        />
+        <div className="p-4">
+          <h2 className="text-xl font-semibold text-white mb-2">
+            {category.name}
+          </h2>
+          {/* <p className="text-sm text-gray-200">{category.description}</p> */}
+        </div>
       </div>
     </Link>
   );
